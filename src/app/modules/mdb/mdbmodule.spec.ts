28e9d9c137e6ce05181b12cbebb71ff2491bdb75
DESKTOP-KJNO7KJ\Ilko Adamov <ilko.z.adamov@gmail.com>
2022-11-24 03:01:08 +0200

    create Circle CI pipeline and bug fixing

--- src/app/modules/mdb/mdbmodule.spec.ts
+++
@@ -1,7 +0,0 @@
-// import { MDBModule } from './MDBModule';
-
-// describe('MDBBootstrapModule', () => {
-//   it('should create an instance', () => {
-//     expect(new MDBModule()).toBeTruthy();
-//   });
-// });
